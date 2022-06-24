244414790ac4ea994a020d33e6486382d946272f
Ray <raymondly96@gmail.com>
2022-06-24 11:48:02 -0700

    Add button styles

--- app.js
+++ app.js
@@ -1,5 +1,34 @@
 let myLibrary = [];
-const newBookBtn = document.querySelector('.btn-newBook');
+const formSubmit = document.querySelector('.form__submit');
+const btnNewBook = document.querySelector('.btn-newBook');
+const title = document.querySelector('.form__input--title');
+const author = document.querySelector('.form__input--author');
+const pages = document.querySelector('.form__input--pages');
+const checkbox = document.querySelector('.form__input--checkbox');
+const popup = document.querySelector('.popup');
+const popupBackground = document.querySelector('.popup__background');
+const form = document.querySelector('.form');
+const popupClose = document.querySelector('.popup__close');
+
+const closePopup = () => {
+    popup.style.zIndex = '-1';
+    popupBackground.style.opacity = '0';
+    form.style.opacity = '0';
+}
+
+const openPopup = () => {
+    popup.style.zIndex = '1';
+    popupBackground.style.opacity = '.9';
+    form.style.opacity = '1';
+}
+
+const clearForm = () => {
+    const inputs = document.querySelectorAll('.form__input');
+    inputs.forEach(input => {
+        input.value = ""
+        input.checked = false;
+    })
+}
 
 class Book{
     constructor(title, author, pages, isRead){
@@ -14,7 +43,8 @@ const harryPotter = new Book('Harry Potter', 'J.K. Rowling', 300, true);
 const harryPotter2 = new Book('Harry Potter 2', 'J.K. Rowling', 300, true);
 const harryPotter3 = new Book('Harry Potter 3', 'J.K. Rowling', 300, true);
 
-const addBooktoLibrary = (newBook) =>{
+const addBooktoLibrary = () =>{
+    const newBook = new Book(title.value, author.value, pages.value, isRead.checked)
     myLibrary.push(newBook);
 }
 
@@ -24,7 +54,18 @@ const showBooks = () => {
     }
 }
 
-newBookBtn,addEventListener('click', ()=>{
-    
+formSubmit.addEventListener('click', (e)=>{
+    e.preventDefault();
+    addBooktoLibrary();
+    clearForm();
+    closePopup();
+})
+
+btnNewBook.addEventListener('click', () => {
+    openPopup();
+})
+
+popupClose.addEventListener('click', () => {
+    closePopup();
 })
 
